7edc87ed6b36dd6a7487ac0e80ae9aca98a05e76
schroederr22 <schroederr22@greenhill.org>
2019-05-01 13:28:53 -0500

    Ryan Schroeder     Challenge 1: Project Hourglass     27 March 2019     v1.0

--- src/fileReader.java
+++ src/fileReader.java
@@ -2,9 +2,35 @@ import java.io.*;
 import java.util.Scanner;
 
 public class fileReader {
-    public static void main(String[] args)throws IOException {
-        File fileInput = new File("input.txt");
-        Scanner scan = new Scanner(fileInput);
+    public static void main(String[] args) throws IOException {
+        File fileInput = new File("C:\\Users\\ryan_\\IdeaProjects\\BeginningJava\\src\\input");
+        Scanner input = new Scanner(fileInput);
+        int count = 0;
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            Scanner forLine = new Scanner(line);
+//            while (forLine.hasNext()){
+//                String word = forLine.next();
+//                if (word.equalsIgnoreCase("allonsy")){
+//                    count ++;
+//                }
+//            }
+            while (forLine.hasNext()){
 
+                String word = forLine.next();
+                Scanner forWord = new Scanner(word);
+
+                    while (forWord.hasNextInt()){
+
+                        int number = forWord.nextInt();
+                        count ++;
+                        System.out.println(number);
+
+                    }
+            }
+            System.out.println(line);
+        }
+        //System.out.printf("There were %d \"allonsy\"s in the file.",count);
+        System.out.printf("There were %d numbers in the file",count);
     }
 }
